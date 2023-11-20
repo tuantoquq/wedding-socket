ec14c198f7e16591e84462be1361fd379d65661f
Nguyễn Hoàng Anh Tuấn <tuantoquq@gmail.com>
2023-11-20 09:56:45 +0700

    add cors for socket io

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: '*' }));
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
 
 io.on('connection', (socket) => {
   console.log('User connected');
